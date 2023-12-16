c8e1fff813f50b874e6eae94f923d564851187b6
Harshit Pant <aaryanpandit3527@gmail.com>
2023-12-16 21:56:39 +0530

    Refactor Tic Tac Toe game UI and functionality

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# Tic-Tac-Toe
+# Tic Tac Toe Game
+
+Welcome to the Tic Tac Toe game! This simple web-based game allows players to take turns marking spaces on a 3x3 grid, aiming to achieve a winning pattern.
+
+## Demo
+[Play the game](#) 
+
+![Tic Tac Toe Screenshot](img/Visual.png) 
+![Winner UI](img/winner.png)
+
+## How to Play
+- Click on the squares to make your move.
+- The game alternates between Player X and Player O.
+- Achieve a winning pattern to win the game.
+
+## Future Upgrades
+Consider the following enhancements for future upgrades:
+- **Improved Styling:** Enhance the visual appeal of the game with additional styling and animations.
+- **Multiplayer Mode:** Implement a multiplayer mode, allowing two players to play against each other.
+- **Difficulty Levels:** Add difficulty levels for single-player mode, with varying AI capabilities.
+- **Scoreboard:** Implement a scoreboard to keep track of the number of wins for each player.
+- **Glich:** Sometimes without filling all the block system declare a Draw. 
+
+## Deployed Version
+The game is deployed and accessible [here](#).
+
+## Setup Locally
+1. Clone the repository.
+2. Open `index.html` in your web browser.
+
+## Technologies Used
+- HTML
+- CSS
+- JavaScript
+
+## Acknowledgments
+This project was inspired by the classic Tic Tac Toe game and built for educational purposes.
+
+Feel free to contribute or provide feedback!
+
+## धन्यवाद
+
---
+++ app.js
@@ -0,0 +1,107 @@
+//Define
+const boxes = document.querySelectorAll(".box");
+const resetBtn = document.querySelector(".reset");
+const dialog = document.getElementById("winnerDialog");
+const winnerMessageElement = document.getElementById("winnerMessage");
+const closeDialogButton = document.getElementById("closeDialog");
+
+
+// Define game Variable
+let yourTurn = true; //playerX and playerO
+// let visited = [];
+const winPtn = [    //winning patterns
+    [0,1,2], 
+    [3,4,5], 
+    [6,7,8],
+    [0,3,6], 
+    [1,4,7], 
+    [2,5,8],
+    [0,4,8], 
+    [2,4,6]
+]
+let count = 0;
+
+resetBtn.addEventListener("click", reset);
+
+boxes.forEach((box,index)=> {
+    box.addEventListener('click', ()=> mark(index));
+});
+
+function mark(idx) {
+    if (yourTurn) {
+        boxes[idx].textContent = "X";
+        boxes[idx].style.color = 'tomato';
+        yourTurn = !yourTurn;
+    } else {
+        boxes[idx].textContent = "O";
+        boxes[idx].style.color = 'green';
+        yourTurn = !yourTurn;
+    }
+    boxes[idx].disabled = true;
+    count++;
+    let winner = checkWinner();
+
+    if (count===9 && !winner) draw();
+}
+
+///// A different Method \\\\\
+// function mark(idx) {
+//     if (!visited.includes(idx)) {   //to prevent re-marking of box
+//         visited.push(idx);
+//         if (yourTurn) {
+//             boxes[idx].textContent = "X";
+//             yourTurn = !yourTurn;
+//         } else {
+//             boxes[idx].textContent = "O";
+//             yourTurn = !yourTurn;
+//         }
+//         boxes[idx].disabled = true;
+//     }
+// }
+
+function checkWinner() {
+    for (let pattern of winPtn) {
+        let pos1 = boxes[pattern[0]].textContent;
+        let pos2 = boxes[pattern[1]].textContent;
+        let pos3 = boxes[pattern[2]].textContent;
+        
+        if (pos1 !== "" && pos2 !== "" && pos3 !== "") {
+            if (pos1 === pos2 && pos2 === pos3) {
+                if (pos1 === "X") { var win = "Player 1"; }
+                else if (pos1 === "O") { var win = "Player 2"; }
+
+                showWinner(win);
+            }
+        }
+    }
+}
+
+function showWinner(win){
+    const winnerMessage = `🎊 Congratulation Winner is ${win} 🫵🏽`;
+
+    winnerMessageElement.textContent = winnerMessage;
+    dialog.showModal();
+    // console.log(winnerMessage);    
+}
+
+function draw(){
+    const winnerMessage = `We have No winner This Time 🤝.    Try Again! `
+
+    winnerMessageElement.textContent = winnerMessage;
+    dialog.showModal();
+    // console.log(winnerMessage);  
+}
+
+// Closing the dialog when the "Close" button is clicked
+closeDialogButton.addEventListener("click", () => {
+    dialog.close();
+    reset();
+});
+
+function reset(){
+    boxes.forEach((box)=> {
+        box.textContent = "";
+        box.disabled = false;
+        yourTurn = true;
+    })
+}
